f6a8b91da7f92533901e2fc52b257cd8cf7f7e81
federicoceli <fed.celi@gmail.com>
2017-08-11 13:26:53 +0200

    Added dummy file

--- HyMPaCt_data_mng.py
+++ HyMPaCt_data_mng.py
@@ -11,10 +11,14 @@ import os.path
 # Valid file to process, flag
 valid_file = False
 
+# Change to local know folder
+path = "C:\\Users\\fedce\\Desktop\\"
+file = "HyMPaCt dummy file.xlsx"
+
 # Wait till user prompts a valid file
 while valid_file == False :     
     # Prompt user for file to process
-    user_file = input("What file should I work on?\n")
+    user_file = input("What file should I work on? (Leave blanc for DEMO)\n")
 
     # Clear the input from quotation marks
     user_file_cleared = user_file[1:-1]
@@ -25,49 +29,77 @@ while valid_file == False :
     # Just a friendly prompt
     if valid_file == True :
         print("Sure, let me work on that!\n")
+    elif user_file == '' :
+        print("Going to use dummy file")
+        user_file_cleared = path + file
+        valid_file = True
     else:
         print("Sorry, does not look right to me!\n")
-
-# path = "C:\\Users\\fedce\\Desktop\\Hympact Log\\"
-# file = "2017-07-20 190400.xlsx"
 
 # Import data from excel file
 data = pd.read_excel(user_file_cleared)
 
 # Create a variable for the single columns
-time = data['Time']
-temp1 = data['Temp1']
-temp2 = data['Temp2']
+time    = data['Time']
+temp1   = data['Temp1']
+temp2   = data['Temp2']
+temp3   = data['Temp3']
+volt    = data['Voltage']
+current = data['Current']
 
 # Use better looking style
 style.use('seaborn-white')
 
 # Create a figure
-fig = plt.figure()
+f, ((plot_temp1, plot_temp2), (plot_voltage, plot_current)) = plt.subplots(2, 2)
 
 # Sub plot 1
-ax1 = fig.add_subplot(211)
+plot_temp1.plot(time, temp1)
 
-ax1.plot(time, temp1)
+plot_temp1.set_xlabel('Time')
+plot_temp1.set_ylabel('Temperature [°C]')
+plot_temp1.set_title('Temperature #1')
 
-plt.ylabel('Temperature [°C]')
-plt.title('Temperature1')
-
-plt.grid(True)
-
-# Sub plot 2
-ax2 = fig.add_subplot(212)
-
-ax2.plot(time, temp2)
-
-plt.xlabel('Time')
-plt.ylabel('Temperature [°C]')
-plt.title('Temperature2')
-
-for label in ax2.xaxis.get_ticklabels():
+for label in plot_temp1.xaxis.get_ticklabels():
     label.set_rotation(45)
 
-plt.grid(True)
+plot_temp1.grid(True)
+
+# Sub plot 2
+plot_temp2.plot(time, temp2)
+
+plot_temp2.set_xlabel('Time')
+plot_temp2.set_ylabel('Temperature [°C]')
+plot_temp2.set_title('Temperature #2')
+
+for label in plot_temp2.xaxis.get_ticklabels():
+    label.set_rotation(45)
+
+plot_temp2.grid(True)
+
+# Sub plot 3
+plot_voltage.plot(time, volt)
+
+plot_voltage.set_xlabel('Time')
+plot_voltage.set_ylabel('Tension [°V]')
+plot_voltage.set_title('TEC Voltage')
+
+for label in plot_voltage.xaxis.get_ticklabels():
+    label.set_rotation(45)
+
+plot_voltage.grid(True)
+
+# Sub plot 4
+plot_current.plot(time, current)
+
+plot_current.set_xlabel('Time')
+plot_current.set_ylabel('Current [A]')
+plot_current.set_title('TEC Current')
+
+for label in plot_current.xaxis.get_ticklabels():
+    label.set_rotation(45)
+
+plot_current.grid(True)
 
 # Show the plot
 plt.tight_layout()
