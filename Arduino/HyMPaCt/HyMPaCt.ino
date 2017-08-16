b251bb147787146bbf5de5f6e81689ce1a308f11
federicoceli <fed.celi@gmail.com>
2017-08-16 17:09:57 +0200

    Better indentation

--- Arduino/HyMPaCt/HyMPaCt.ino
+++ Arduino/HyMPaCt/HyMPaCt.ino
@@ -39,39 +39,37 @@ uint16_t calculateCheckSum(unsigned char *buf, unsigned int n) {
 }
 
 /**** CRC calculation ****/
-uint16_t calculateCRC(const uint8_t *data, uint16_t size)
-{
-  uint16_t out = 0;
-  int bits_read = 0, bit_flag;
+uint16_t calculateCRC(const uint8_t *data, uint16_t size){
+    uint16_t out = 0;
+    int bits_read = 0, bit_flag;
 
-  /* Sanity check: */
-  if (data == NULL)
-    return 0;
+    /* Sanity check: */
+    if (data == NULL)
+        return 0;
 
-  while (size > 0)
-  {
-    bit_flag = out >> 15;
+    while (size > 0) {
+        bit_flag = out >> 15;
 
-    /* Get next bit: */
-    out <<= 1;
-    out |= (*data >> (7 - bits_read)) & 1;
+        /* Get next bit: */
+        out <<= 1;
+        out |= (*data >> (7 - bits_read)) & 1;
 
-    /* Increment bit counter: */
-    bits_read++;
-    if (bits_read > 7)
-    {
-      bits_read = 0;
-      data++;
-      size--;
+        /* Increment bit counter: */
+        bits_read++;
+        if (bits_read > 7)
+        {
+            bits_read = 0;
+            data++;
+            size--;
+        }
+            
+        /* Cycle check: */
+        if (bit_flag) {
+            out ^= CRC16;
+        }
+
     }
-    
-    /* Cycle check: */
-    if (bit_flag) {
-      out ^= CRC16;
-    }
-
-  }
-  return out;
+    return out;
 }
 
 /**** Reads temperature from RTD ****/
@@ -91,101 +89,101 @@ int readTemprature(MAX31865_RTD rtd) {
 
 /**** Generates a randomly populated array ****/
 void rndArray(int* dummyArray, int lenght, int min_v, int max_v) {
-  for (int i = 0; i < lenght; i++) {
-    dummyArray[i] = random(min_v, max_v);
-  }
+    for (int i = 0; i < lenght; i++) {
+        dummyArray[i] = random(min_v, max_v);
+    }
 }
 
 /**** Assembles a full packet of given type and value(s) ****/
 void pktAssemble(unsigned char* packet, int type, int value[]) {
-  uint16_t  checkSum = 0;
-  uint16_t  crc = 0;
+    uint16_t  checkSum = 0;
+    uint16_t  crc = 0;
 
-  int       temp1;
-  
-  // synchronization word   [0-1]
-  packet[0] = SYNC_0;
-  packet[1] = SYNC_1;
+    int       temp1;
+    
+    // synchronization word   [0-1]
+    packet[0] = SYNC_0;
+    packet[1] = SYNC_1;
 
-  // message type       [2]
-  packet[2] = type;
+    // message type       [2]
+    packet[2] = type;
 
-  // sequence         [3-4]
-  packet[3] = (char)((seq_number[type]) >> 8);  
-  packet[4] = (char)(seq_number[type]++);
+    // sequence         [3-4]
+    packet[3] = (char)((seq_number[type]) >> 8);  
+    packet[4] = (char)(seq_number[type]++);
 
-  // data (16 sets of 2 byte) [5-36]
-  if (type == HYMPACT) {
-    // Read from RTD and insert into packet
-    // TODO if this works it has to be moved, tho
-    temp1 = readTemprature(rtd1);
-    packet[5] = (char)((temp1) >> 8);
-    packet[6] = (char)(temp1);
+    // data (16 sets of 2 byte) [5-36]
+    if (type == HYMPACT) {
+        // Read from RTD and insert into packet
+        // TODO if this works it has to be moved, tho
+        temp1 = readTemprature(rtd1);
+        packet[5] = (char)((temp1) >> 8);
+        packet[6] = (char)(temp1);
 
-    for (int set = 1; set < 16; set++)
-        for (int byte = 0; byte < 2; byte++)
-          packet[5 + set * 2 + byte] = (char)((value[set] >> 8 * (1 - byte)));
-  }
+        for (int set = 1; set < 16; set++)
+            for (int byte = 0; byte < 2; byte++)
+            packet[5 + set * 2 + byte] = (char)((value[set] >> 8 * (1 - byte)));
+    }
 
-  // data (16 sets of 2 byte) [5-36]
-  else {
-    for (int set = 0; set < 8; set++)
-        for (int byte = 0; byte < 4; byte++)
-        packet[5 + set * 4 + byte] = (char)((value[set] >> 8 * (3 - byte)));
-  }
+    // data (16 sets of 2 byte) [5-36]
+    else {
+        for (int set = 0; set < 8; set++)
+            for (int byte = 0; byte < 4; byte++)
+            packet[5 + set * 4 + byte] = (char)((value[set] >> 8 * (3 - byte)));
+    }
 
-  // checksum         [37-38]
-  checkSum = calculateCheckSum(packet, PKTL - 4);
-  packet[37] = (char)((checkSum) >> 8);
-  packet[38] = (char)(checkSum);
+    // checksum         [37-38]
+    checkSum = calculateCheckSum(packet, PKTL - 4);
+    packet[37] = (char)((checkSum) >> 8);
+    packet[38] = (char)(checkSum);
 
-  // crc            [39-40]
-  crc = calculateCRC(packet, PKTL - 2);
-  packet[39] = (char)((crc) >> 8);
-  packet[40] = (char)(crc);
-  //cout << "CRC = " << crc << "; [39] = " << (int)packet[39] << ", [40] = " << (int)packet[40] << "\n";
+    // crc            [39-40]
+    crc = calculateCRC(packet, PKTL - 2);
+    packet[39] = (char)((crc) >> 8);
+    packet[40] = (char)(crc);
+    //cout << "CRC = " << crc << "; [39] = " << (int)packet[39] << ", [40] = " << (int)packet[40] << "\n";
 }
 
 
 void setup() {
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  /* Initialize SPI communication. */
-  SPI.begin();
-  SPI.setClockDivider(SPI_CLOCK_DIV16);
-  SPI.setDataMode(SPI_MODE3);
-  
-  /* Allow the MAX31865 to warm up. */
-  delay(100);
-  
-  /* Configure:
-       V_BIAS enabled
-       Auto-conversion
-       1-shot disabled
-       3-wire enabled
-       Fault detection:  automatic delay
-       Fault status:  auto-clear
-       50 Hz filter
-       Low threshold:  0x0000
-       High threshold:  0x7fff
-  */
-  
-  rtd1.configure(true, true, false, true, MAX31865_FAULT_DETECTION_NONE,
-                 true, true, 0x0000, 0x7fff);
-  rtd2.configure(true, true, false, true, MAX31865_FAULT_DETECTION_NONE,
-                 true, true, 0x0000, 0x7fff);
-  rtd3.configure(true, true, false, true, MAX31865_FAULT_DETECTION_NONE,
-                 true, true, 0x0000, 0x7fff);               
-}
+    /* Initialize SPI communication. */
+    SPI.begin();
+    SPI.setClockDivider(SPI_CLOCK_DIV16);
+    SPI.setDataMode(SPI_MODE3);
+    
+    /* Allow the MAX31865 to warm up. */
+    delay(100);
+    
+    /* Configure:
+        V_BIAS enabled
+        Auto-conversion
+        1-shot disabled
+        3-wire enabled
+        Fault detection:  automatic delay
+        Fault status:  auto-clear
+        50 Hz filter
+        Low threshold:  0x0000
+        High threshold:  0x7fff
+    */
+    
+    rtd1.configure(true, true, false, true, MAX31865_FAULT_DETECTION_NONE,
+                    true, true, 0x0000, 0x7fff);
+    rtd2.configure(true, true, false, true, MAX31865_FAULT_DETECTION_NONE,
+                    true, true, 0x0000, 0x7fff);
+    rtd3.configure(true, true, false, true, MAX31865_FAULT_DETECTION_NONE,
+                    true, true, 0x0000, 0x7fff);               
+ }
 
 void loop() {
-  rndArray(tempArray, 16, 20, 30);
-  pktAssemble(packet, HYMPACT, tempArray);
-  //Serial.write(packet, PKTL);
-  for( int n = 0; n < PKTL; n++ )
-  {
+    rndArray(tempArray, 16, 20, 30);
+    pktAssemble(packet, HYMPACT, tempArray);
+    //Serial.write(packet, PKTL);
+    for( int n = 0; n < PKTL; n++ )
+    {
     Serial.write(packet[n]);
-  }
-  
-  delay(50);
+    }
+
+    delay(50);
 }
